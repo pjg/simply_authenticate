369b400fbcad4e1157909adfd70580ee243f7df5
Paweł Gościcki <pawel.goscicki@gmail.com>
2009-01-24 11:58:39 +0100

    Added 'login_required' helper method and the accompanying 'redirect_to_stored'.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,8 +2,17 @@ module SimplyAuthenticate
 
   module Helpers
 
+    # HELPER
     def logged_in?
       !session[:user_id].blank?
+    end
+
+    # FILTER
+    def login_required
+      return if logged_in?
+      flash[:warning] = 'Proszę się zalogować'
+      session[:return_to] = request.request_uri
+      redirect_to login_path
     end
 
     # FILTER: user is loaded for every page view
@@ -17,6 +26,16 @@ module SimplyAuthenticate
       @current_user = User.find_by_autologin_token(cookies[:autologin_token])
       if @current_user && !@current_user.autologin_expires.nil? && Time.now < @current_user.autologin_expires
         session[:user_id] = @current_user.id
+      end
+    end
+
+    # ACTION
+    def redirect_to_stored
+      if return_to = session[:return_to] then
+        session[:return_to] = nil
+        redirect_to return_to
+      else
+        redirect_to root_path
       end
     end
 
